18a78e5aebea4ceb68473616ecd1020a81f35d75
Maxim Nikolaev <darwinnnn27@gmail.com>
2023-07-30 20:17:20 +0300

    frontend deploy

--- backend/allowedCORS/allowedCORS.js
+++ backend/allowedCORS/allowedCORS.js
@@ -2,4 +2,5 @@ module.exports = [
   'http://localhost:3001',
   'http://nikolaev-maxim-mesto.nomoreparties.co',
   'https://nikolaev-maxim-mesto.nomoreparties.co',
+  'http://nikolaev-maxim-mesto.nomoreparties.co/',
 ];
